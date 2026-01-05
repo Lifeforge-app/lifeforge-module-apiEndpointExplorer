b2a6585f9183fbb33fe6b3c4e080af2f132f23f7
Melvin Chia <melvinchia623600@gmail.com>
2026-01-05 12:21:54 +0800

    feat: update to latest version from forgistry

--- manifest.ts
+++ manifest.ts
@@ -2,10 +2,7 @@ import { lazy } from 'react'
 import type { ModuleConfig } from 'shared'
 
 export default {
-  name: 'API Endpoint Explorer',
-  icon: 'mynaui:api',
   routes: {
     '/': lazy(() => import('@'))
   },
-  category: 'Information'
 } satisfies ModuleConfig
